c5c364d9a9a5caff8ce74e08f758fd34744afa4c
namfom317 <mdjdjm317@gmail.com>
2019-09-21 12:15:11 +0700

    1-4

---
+++ 2-if-for.js
@@ -0,0 +1,45 @@
+// const a = "a";
+// if (a === "a") {
+//   console.log("It's a");
+// } else {
+//   console.log("It's not a");
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// let a = 0;
+// while (i < 1001) {
+//   a = a + i;
+//   i++;
+// }
+// console.log(a);
+
+// let h = [];
+// let student = {
+//   id: "60144235",
+//   name: "Sirirat Wichityaem",
+//   faculty: "Science"
+// };
+// console.log(student.name);
+// student.name = "namfon";
+// console.log(student.name);
+
+let h = [];
+let student = {
+  id: "60144235",
+  "name-lastname": "Sirirat Wichityaem",
+  faculty: "Science"
+};
+console.log([name-lastname]);
+[name-lastname] = "namfon";
+console.log([name-lastname]);
